98439b48e55724783a94fae34a6d28c46303955c
Marta <marta_rodrigolloveras@iescarlesvallbona.cat>
2024-09-25 20:03:32 +0200

    Afegim atributs inicials a la classe Tauler (files, columnes etc) i creem el metode initTauler per mostrar el tauler per pantalla

--- src/conecta4_martarodrigo/Tauler.java
+++ src/conecta4_martarodrigo/Tauler.java
@@ -9,5 +9,22 @@ package conecta4_martarodrigo;
  * @author martarodrigo
  */
 public class Tauler {
-    
+    private final char[][] tauler;
+    private final int files;
+    private final int colus;
+
+    public Tauler(int files, int colus) {
+        this.files = files;
+        this.colus = colus;
+        tauler = new char[files][colus];
+        initTauler();
+    }
+
+    private void initTauler() {
+        for (int i = 0; i < files; i++) {
+            for (int j = 0; j < colus; j++) {
+                tauler[i][j] = ' '; 
+            }
+        }
+    }
 }
